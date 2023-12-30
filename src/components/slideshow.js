fc6caa62580e1931ddcc9f1dc5a7b31d865e5f8a
Shreyas <schicker@purdue.edu>
2023-12-29 18:30:53 -0800

    fixed picture filetypes

--- src/components/slideshow.js
+++ src/components/slideshow.js
@@ -16,37 +16,36 @@ const Slideshow = ({message, startDelay}) => {
           chevron_left
         </button> */}
         <ul class={imageList}>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic1.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic2.jpg"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic1.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic2.JPG"/>
             <StaticImage class={imageItem} src="../images/galleryPics/galleryPic3.jpg"/>
-            {/* <StaticImage class={imageItem} src="../images/galleryPics/galleryPic4.jpg"/> */}
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic5.jpg"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic5.JPG"/>
             <StaticImage class={imageItem} src="../images/galleryPics/galleryPic6.jpg"/>
             <StaticImage class={imageItem} src="../images/galleryPics/galleryPic7.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic8.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic9.jpg"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic8.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic9.PNG"/>
             <StaticImage class={imageItem} src="../images/galleryPics/galleryPic10.jpg"/>
             <StaticImage class={imageItem} src="../images/galleryPics/galleryPic11.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic12.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic13.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic14.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic15.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic16.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic17.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic18.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic19.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic20.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic21.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic22.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic23.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic24.jpg"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic12.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic13.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic14.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic15.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic16.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic17.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic18.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic19.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic20.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic21.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic22.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic23.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic24.JPG"/>
             <StaticImage class={imageItem} src="../images/galleryPics/galleryPic25.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic26.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic27.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic28.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic29.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic30.jpg"/>
-            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic31.jpg"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic26.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic27.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic28.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic29.PNG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic30.JPG"/>
+            <StaticImage class={imageItem} src="../images/galleryPics/galleryPic31.JPG"/>
             
         </ul>
         {/* <button id="next-slide" class={`slideButton materialSymbolsRounded`}>
